0e92d86ab5ca7f086c29b30007d4b50bad510d9b
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-09-19 23:57:29 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import json
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    #if request.method == "POST":
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
