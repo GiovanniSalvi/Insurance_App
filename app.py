78f3155eb32a723912964d98d0e02c2b043ed58a
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-09-20 18:30:06 +0000

    four commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 import json
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
+from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
+from pydantic import ValidationError
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
@@ -10,26 +14,38 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/")
+@app.route("/index/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        sender = {
-            'senderName': request.form.get('Sender_Name'),
-            'address': request.form.get('Sender_Address'),
-            'city': request.form.get('Sender_City'),
-            'country': request.form.get('Sender_Country')
-        }
-        mongo.db.Package.insert_one(sender)
-        
+        package = {
+                'senderName': request.form.get('Sender_Name'),
+                'senderAddress': request.form.get('Sender_Address'),
+                'senderCity': request.form.get('Sender_City'),
+                'senderCountry': request.form.get('Sender_Country'),
+                'recipientName': request.form.get('Recipient_Name'),
+                'recipientAddress': request.form.get('Recipient_Address'),
+                'recipientCity': request.form.get('Recipient_City'),
+                'recipientCountry': request.form.get('Recipient_Country'),
+                'despatchDate': request.form.get('Date'),
+                'value': request.form.get('Value'),
+                'content': request.form.get('Content'),
+                'insuranceProvived': request.form.get('Insurance'),
+                'trackingNumber': request.form.get('Tracking_Number')
+            }
+        mongo.db.Package.insert_one(package)
+        print('inserted')
+        return redirect(url_for('orders'))
     return render_template("index.html")
 
 
-@app.route("/Orders/", methods=["GET", "POST"])
-def orders():
-
-    return render_template("orders.html")
-
-
+@app.route("/Orders/<order>", methods=["GET, POST"])
+def orders(order):
+    if request.method == "POST":
+        order = mongo.db.Package.find_one({"_id": ObjectId(order)})
+        return render_template("orders.html", order = order)
+  
 
 if __name__ == "__main__":
     app.run(
