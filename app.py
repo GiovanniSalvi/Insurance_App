db868a76e3e79f08f7a3e4dc1da245093a7b822d
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-09-20 13:31:35 +0000

    third commit

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-@app.route("/")
+mongo = PyMongo(app)
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    #if request.method == "POST":
+    if request.method == "POST":
+        sender = {
+            'senderName': request.form.get('Sender_Name'),
+            'address': request.form.get('Sender_Address'),
+            'city': request.form.get('Sender_City'),
+            'country': request.form.get('Sender_Country')
+        }
+        mongo.db.Package.insert_one(sender)
+        
     return render_template("index.html")
 
 
-@app.route("/Orders/", methods=["GET"])
+@app.route("/Orders/", methods=["GET", "POST"])
 def orders():
 
     return render_template("orders.html")
+
 
 
 if __name__ == "__main__":
