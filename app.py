849c27c25c3e216f37ffbc82ac907c8cf668e89a
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-09-21 01:27:25 +0000

    fifth commit

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, url_for, request, redirect
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 from pydantic import ValidationError
+import binascii
+import datetime
 if os.path.exists("env.py"):
     import env
 
@@ -19,6 +21,38 @@ mongo = PyMongo(app)
 @app.route("/index/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        value = int(request.form.get('Value'))
+        print(value)
+        despatchDate = request.form.get('Date')
+        recipientCountry = request.form.get('Recipient_Country').upper()
+        insuranceProvided = request.form.get('Insurance')
+        print(insuranceProvided)
+        insuranceCharge = 0.00
+        trackingNumber = binascii.b2a_hex(os.urandom(15))
+        print(trackingNumber)
+        existing_trackingNumber = mongo.db.Package.find_one(
+            {"trackingNumber": "trackingNumber"}
+        )
+        acceptedAt = datetime.datetime.now()
+        print(acceptedAt)
+        if value < 10000 and existing_trackingNumber is False:
+            if  insuranceProvided == 'True':
+                print('true')
+                if recipientCountry == 'GB':
+                    print(recipientCountry)
+                    insuranceCharge = round(value * 0.01, 3)
+                    print(insuranceCharge)                       
+                    if recipientCountry == 'DE' or recipientCountry == 'NE' or recipientCountry == 'FR':
+                        insuranceCharge = round(value * 0.015, 3)
+                    else:
+                        insuranceCharge = round(value * 0.04, 3)                        
+                    if  insuranceCharge < 9.00:
+                            insuranceCharge = 9.00   
+            else:   
+                insuranceCharge = 0.00
+                insuranceProvided == 'False'
+        ipt = round(insuranceCharge * 0.12, 3) 
+
         package = {
                 'senderName': request.form.get('Sender_Name'),
                 'senderAddress': request.form.get('Sender_Address'),
@@ -27,24 +61,28 @@ def index():
                 'recipientName': request.form.get('Recipient_Name'),
                 'recipientAddress': request.form.get('Recipient_Address'),
                 'recipientCity': request.form.get('Recipient_City'),
-                'recipientCountry': request.form.get('Recipient_Country'),
-                'despatchDate': request.form.get('Date'),
-                'value': request.form.get('Value'),
+                'recipientCountry': recipientCountry,
+                'value': value,
                 'content': request.form.get('Content'),
-                'insuranceProvived': request.form.get('Insurance'),
-                'trackingNumber': request.form.get('Tracking_Number')
-            }
-        mongo.db.Package.insert_one(package)
-        print('inserted')
-        return redirect(url_for('orders'))
+                'insuranceProvived': insuranceProvided,
+                'insuranceCharge': insuranceCharge,
+                'ipt': ipt,
+                'trackingNumber': trackingNumber,
+                'despatchDate': despatchDate,
+                #'orderURL': orderURL,
+                'acceptedAt': acceptedAt
+            }  
+        order = mongo.db.Package.insert_one(package)       
+        return redirect(url_for('orders', order = order.inserted_id))
+        
     return render_template("index.html")
 
 
-@app.route("/Orders/<order>", methods=["GET, POST"])
+@app.route("/Orders/<order>", methods=["GET"])
 def orders(order):
-    if request.method == "POST":
-        order = mongo.db.Package.find_one({"_id": ObjectId(order)})
-        return render_template("orders.html", order = order)
+
+    order = mongo.db.Package.find_one({"_id": ObjectId(order)})
+    return render_template("orders.html", order = order)
   
 
 if __name__ == "__main__":
